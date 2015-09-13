648f56b1af72f2f3246520af0905fe48d94775ea
Yamir Ramirez <ysramirez@gmail.com>
2015-09-13 09:51:19 -0500

    fix routes

--- src/AppBundle/Controller/MinicartController.php
+++ src/AppBundle/Controller/MinicartController.php
@@ -9,17 +9,21 @@ class MinicartController extends Controller
 {
     public function indexAction()
     {
-        $products = $this->getDoctrine()->getManager()->getRepository('AppBundle:Product')->findAll();
-
-        return $this->render('app/index.html.twig', array(
-            'products' => $products
-        ));
+        return $this->render(
+            'app/index.html.twig',
+            array(
+                'products' => $this->getDoctrine()->getManager()->getRepository('AppBundle:Product')->findAll()
+            )
+        );
     }
 
     public function showAction(Product $product)
     {
-        return $this->render('app/show.html.twig', array(
-            'product' => $product
-        ));
+        return $this->render(
+            'app/show.html.twig',
+            array(
+                'product' => $product
+            )
+        );
     }
 }
