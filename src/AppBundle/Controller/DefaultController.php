35a3126682b04f5da5e4d0c69de8024e41208aac
Yamir Ramirez <ysramirez@gmail.com>
2015-09-13 09:29:38 -0500

    fix routing

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,14 +3,10 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Product;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
     public function indexAction()
     {
         $products = $this->getDoctrine()->getManager()->getRepository('AppBundle:Product')->findAll();
@@ -20,9 +16,6 @@ class DefaultController extends Controller
         ));
     }
 
-    /**
-     * @Route("/show/{id}", name="show-product")
-     */
     public function showAction(Product $product)
     {
         return $this->render('app/show.html.twig', array(
