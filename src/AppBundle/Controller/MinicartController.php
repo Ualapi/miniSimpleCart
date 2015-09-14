d37d5dcee4e04d15edddf68cb1809e53df51188b
Yamir Ramirez <ysramirez@gmail.com>
2015-09-14 01:55:55 -0500

    add product

--- src/AppBundle/Controller/MinicartController.php
+++ src/AppBundle/Controller/MinicartController.php
@@ -4,27 +4,47 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Product;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\BrowserKit\Request;
 
 class MinicartController extends Controller
 {
     public function indexAction()
     {
-        if (count($this->get('session')->get('cart_products')) > 0) {
+        if (count($this->get('session')->get('cart')) > 0) {
 
         }
 
         return $this->render(
             'AppBundle:app:index.html.twig',
-            array(
+            [
                 'products' => $products = $this->getDoctrine()->getManager()->getRepository('AppBundle:Product')->findAll(),
-                'formProducts' => $this->getFormProductsViews($products),
-            )
+                'formProducts' => $this->getFormProductsViews($this->getFormProducts($products)),
+            ]
         );
     }
 
-    public function addAction()
+    public function addAction(Request $request, $id)
     {
+        $cart = $this->get('session')->has('cart') ? $this->get('session')->get('cart') : [];
 
+        $form = $this->getFormProducts(
+            $product = $this->getDoctrine()->getManager()->getRepository('AppBundle:Product')->findOneBy(['id' => $id])
+        );
+
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $cart[$product->getId()] =
+                [
+                    'name' => $product->getName(),
+                    'price' => $product->getPrice(),
+                    'quantity' => $form->get('quantity')->getData()
+                ]
+            ;
+        }
+
+        $cart[$product->getId()] = $product;
+        $this->get('session')->set('cart', $cart);
     }
 
     public function checkoutAction()
@@ -47,15 +67,25 @@ class MinicartController extends Controller
         );
     }
 
-    private function getFormProductsViews($products)
+    private function getFormProducts($products)
     {
+        $formProducts = [];
         foreach ($products  as $key => $product) {
-            $formProductsViews[$key] = $this->createFormBuilder()
-                                       ->setAction('asdf')
-                                       ->add('quantity', 'integer')
-                                       ->add('add', 'submit', array('label' => 'add'))
-                                       ->getForm()
-                                       ->createView();
+            $formProducts[$key] = $this->createFormBuilder()
+                                            ->setAction($this->generateUrl('app_add_product', ['id' => $product->getId()]))
+                                            ->add('quantity', 'integer')
+                                            ->add('add', 'submit', ['label' => 'add'])
+                                            ->getForm()
+            ;
+        }
+        return $formProducts;
+    }
+
+    private function getFormProductsViews($formProducts)
+    {
+        $formProductsViews = [];
+        foreach ($formProducts  as $key => $form) {
+            $formProductsViews[$key] = $form->createView();
         }
         return $formProductsViews;
     }
