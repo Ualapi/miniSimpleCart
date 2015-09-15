cf3a9712a0026f0c56d73d658fa8219b06a37b64
Juan Luis García Borrego <juanluisgarciaborrego@gmail.com>
2015-09-15 08:46:26 +0200

    Load Fixtures fix

--- src/AppBundle/DataFixtures/ORM/LoadProductsData.php
+++ src/AppBundle/DataFixtures/ORM/LoadProductsData.php
@@ -21,7 +21,7 @@ class LoadProductsData implements FixtureInterface
             $product->setName($item[0]);
             $product->setDescription($item[1]);
             $product->setPrice($item[2]);
-            $product->setImage($item[5]);
+            $product->setImage($item[3]);
 
             $manager->persist($product);
         }
