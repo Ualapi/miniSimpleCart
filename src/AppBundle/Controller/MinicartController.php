64b5e7a5d247dcced5b75a38e24761312199e8a3
Juan Luis <juanluisgarciaborrego@gmail.com>
2015-09-21 20:21:37 +0200

    view form dinamyc ok

--- src/AppBundle/Controller/MinicartController.php
+++ src/AppBundle/Controller/MinicartController.php
@@ -11,11 +11,21 @@ class MinicartController extends Controller
 {
     public function indexAction()
     {
+        $cart = $this->get('session')->has('cart') ? $this->get('session')->get('cart') : [];
+
+        $builder = $this->createFormBuilder();
+
+        foreach ($cart as $key => $item) {
+            $builder->add($key, 'integer', ['attr' => ['min' => 1, 'max' => 99, 'value' => $item['quantity'] ], 'label' => false, 'mapped' => false]);
+        }
+        $form = $builder->getForm();
+
         return $this->render(
             'AppBundle:app:index.html.twig',
             [
                 'products' => $products = $this->getDoctrine()->getManager()->getRepository('AppBundle:Product')->findAll(),
                 'formProducts' => $this->getFormProductsViews($this->getFormProducts($products)),
+                'form' => $form->createView(),
                 'formProductsCart' =>
                     empty(!$this->get('session')->get('cart')) ? $this->getFormProductsViews($this->getFormProducts($this->get('session')->get('cart'))) : null
             ]
