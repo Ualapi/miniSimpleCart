fb12989b5a6fc391f4d253bdf1e0b09084fe1c15
Juan Luis García Borrego <juanluisgarciaborrego@gmail.com>
2015-09-21 14:16:43 +0200

    quantity mapped = false in form type

--- src/AppBundle/Form/QuantityProductType.php
+++ src/AppBundle/Form/QuantityProductType.php
@@ -10,7 +10,7 @@ class QuantityProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('quantity', 'integer', ['attr' => ['min' => 1, 'max' => 99, 'value' => 1], 'label' => false])
+            ->add('quantity', 'integer', ['attr' => ['min' => 1, 'max' => 99, 'value' => 1], 'label' => false, 'mapped' => false])
             ->add('id', 'hidden', array(
                 'data' => '0'
             ))
